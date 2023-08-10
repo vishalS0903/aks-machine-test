9b8db5bdaf224b762a10690b7d3844c661c38934
Vishal Sharnagat <106508871+vishalS0903@users.noreply.github.com>
2023-08-10 23:37:46 +0530

    added sourcecode.blade.php

---
+++ resources/views/sourcecode.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    {{-- @php
+    $count =1 ;
+@endphp
+{{$count++}} --}}
+{{-- <input type="checkbox" name="status" value="1" @if ($data->status== 1) checked  @endif> --}}
+
+</body>
+</html>
